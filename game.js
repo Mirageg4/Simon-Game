f1bdfa6a4392678e15702800496e390416116f15
John Manouse <jmanouse@mac.com>
2021-04-05 09:50:37 -0400

    button selection & sound

--- game.js
+++ game.js
@@ -1,2 +1,21 @@
+'use strict';
 
+const buttonColors = ['red', 'blue', 'green', 'yellow'];
+
+let gamePattern =[]
+
+// Generate Random # between 0 & 3, and store in randomNumber 
+function nextSequence(){
+    let randomNumber = Math.trunc(Math.random) * 3 + 1;
+
+//the random number above selects a color and adds to gamePattern array
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+//Select the button with the same id as randomChosenColor
+$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+//play sound for button selected
+let audio = newAudio("sounds/" + randomChosenColor + ".mp3"); audio.play();
   
+}
