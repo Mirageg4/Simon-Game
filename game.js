99a373c0b9363aea98f20a2bcb0d2b8891987ee5
John Manouse <jmanouse@mac.com>
2021-04-06 13:13:44 -0400

    game restart

--- game.js
+++ game.js
@@ -42,8 +42,7 @@ $(".btn").click (function() {
 
 function checkAnswer(currentLevel) {
     //Check if most recent user answer is the same as the game pattern. If so, then log "success", otherwise log "wrong".
-    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        console.log("success");
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
         //If the the most recent answer is right above, then check that they have finished their sequence with another if statement.
 
@@ -53,10 +52,23 @@ function checkAnswer(currentLevel) {
         setTimeout(function () {
             nextSequence();
         }, 1000);
+    }
 
-        }
     }   else {
-        console.log("wrong");
+        // plays sound if answer is wrong
+        playSound("wrong");
+
+        //Apply class to the body of the website, when the answer is wrong, and then remove it after 200 milliseconds.
+        $("body").addClass("game-over");
+
+         //Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+         $("level-title").text("Game Over, Press any key to Restart");
+
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
     }
 }
 
@@ -64,14 +76,12 @@ function nextSequence () {
 
     //Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
     userClickedPattern = [];
-
     level++;
     $("#level-title").text("Level " + level);
-}
 
 // Generate Random # between 0 & 3, and store in randomNumber 
-function nextSequence(){
-    let randomNumber = Math.trunc(Math.random) * 3 + 1;
+    //let randomNumber = Math.trunc(Math.random) * 3 + 1;
+    let randomNumber = Math.floor(Math.random() * 4);
 
 //the random number above selects a color and adds to gamePattern array
     let randomChosenColor = buttonColors[randomNumber];
@@ -83,22 +93,28 @@ $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
 playSound(randomChosenColor);
 }
+//add pressed class to clicked button
+function animatePress(currentColor) {
+    $("#" +currentColor).addClass("pressed");
+    
+    //remove pressed class to clicked button after 100 milliseconds
+    setTimeout(function () {
+        $("#" +currentColor).removeClass("pressed");
+    }, 100);
+    }
 
 function playSound(name) {
 //play sound for button selected
 let audio = newAudio("sounds/" + name + ".mp3"); 
 audio.play(); 
 }
-//add pressed class to clicked button
-function animatePress(currentColor) {
-$("#" +currentColor).addClass("pressed");
 
-//remove pressed class to clicked button after 100 milliseconds
-setTimeout(function () {
-    $("#" +currentColor).removeClass("pressed");
-}, 100);
-
+function startOver () {
+    level = 0;
+    gamePattern = [];
+    started =  false;
 }
+
 
 
    
